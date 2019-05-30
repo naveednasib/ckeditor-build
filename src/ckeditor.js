f1f0238267c42a72590225710e8df1ace197438f
mnnasib7777 <mnnasib7777@gmail.com>
2019-05-30 11:53:44 +0500

    adding new features, mention, alignment, underline, Strikethrough

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -5,11 +5,12 @@
 
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/Strikethrough';
+import underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
@@ -28,6 +29,8 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Font from '@ckeditor/ckeditor5-font/src/font';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -54,14 +57,18 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	Font
+	Font,
+	Alignment,
+	Strikethrough,
+	underline,
+	Mention
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
-	
+
 	toolbar: [ 'heading', '|', 'bold', 'italic', 'fontSize' ],
-	
+
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
