9fbe5ee8ca615d55d559afb3a6a8cd3c79e5b2fc
Muhammad naveed nasib <naveed.nasib@goodcoresoft.com>
2020-01-29 18:05:01 +0500

    change

--- tests/ckeditor.js
+++ tests/ckeditor.js
@@ -3,7 +3,7 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/* globals document */
+/* globals document check change*/
 
 import ClassicEditor from '../src/ckeditor';
 import BaseClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
